d6ffe9ce7c07283491838c8d09655bd84772bf83
Alejandro Castro <alejo199821@gmail.com>
2024-05-15 20:34:15 -0400

    complete in class assignment

--- index.php
+++ index.php
@@ -10,14 +10,33 @@
 
     <?php echo '<p>Let\'s learn:</p>'; ?>
    
+    <?php
+        echo 
+        '<ul>
+            <li>PHP</li>
+            <li>Laravel</li>
+         </ul>';
+    ?>
     
-    <ul>
-        <li>PHP</li>
-        <li>Laravel</li>
-    </ul>
+    <!-- BOTH IMG ARE OKAY,SECOND IS PREFERED -->
+    <?php
+        echo ' <img src="https://placehold.co/600x400" alt="">'
+    ?>
+     <img src="<?php echo 'https://placehold.co/600x400' ?>" alt="">
     
+    <?php
+        $fname = "Alejandro";
+        $lname = "Castro";
 
-    <img src="https://placehold.co/600x400" alt="">
+        $name['fname'] = "Alejandro";
+        $name['lname'] = "Castro";
+
+        // $name = array ('Alejandro','Castro')
+        // $name[0];
+
+        echo 'Hello ' . $fname . " " . $lname;
+    ?>
+   
     
 </body>
 </html>
