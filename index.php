71bee415edc2006cc9deab42e17e1220679a4aeb
Alejandro Castro <alejo199821@gmail.com>
2024-05-15 20:11:40 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo '<h1>Welcome to HTTP525</h1>'; ?>
+
+    <?php echo '<p>Let\'s learn:</p>'; ?>
+   
+    
+    <ul>
+        <li>PHP</li>
+        <li>Laravel</li>
+    </ul>
+    
+
+    <img src="https://placehold.co/600x400" alt="">
+    
+</body>
+</html>
